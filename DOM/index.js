8b818616ee8b0dda4c19a6769689135c8756cc8e
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-06 14:17:44 +0300

    learning DOM

---
+++ DOM/index.js
@@ -0,0 +1,6 @@
+document.querySelector("a").style.color = "green";
+document.querySelector("button").style.backgroundColor = "yellow";
+// document.querySelector("h1").classList.toggle("huge")
+document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
+document.querySelector("h1").textContent = "LOL";
+document.querySelector("a").setAttribute("href", "https://www.bing.com")
